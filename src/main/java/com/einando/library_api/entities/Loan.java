7648afd88744b4b038767817a982ed1a73021fc2
Fernando Lopes <87499075+einand0@users.noreply.github.com>
2025-04-15 16:16:09 -0300

    feat: entity Loan created

---
+++ src/main/java/com/einando/library_api/entities/Loan.java
@@ -0,0 +1,26 @@
+package com.einando.library_api.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_loans")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Loan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID loan_id;
+    private Date loanDate;
+    private Date returnDate;
+    private boolean returned;
+}
