a6af877b7eb7e028a335d7a5dec29fe5791b1a64
Fernando Lopes <87499075+einand0@users.noreply.github.com>
2025-04-15 20:15:22 -0300

    add H2 database

--- src/main/java/com/einando/library_api/services/UserService.java
+++ src/main/java/com/einando/library_api/services/UserService.java
@@ -1,9 +1,10 @@
 package com.einando.library_api.services;
 
-import com.einando.library_api.entities.Loan;
 import com.einando.library_api.entities.User;
 import com.einando.library_api.repositories.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -12,5 +13,9 @@ public class UserService {
 
     public User createUser(User user){
         return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 }
