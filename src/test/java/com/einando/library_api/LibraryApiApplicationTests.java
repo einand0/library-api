31c097cca3e6e8384f4c05d938e916b2403d0ac3
Fernando Lopes <87499075+einand0@users.noreply.github.com>
2025-04-14 21:51:41 -0300

    first commit

---
+++ src/test/java/com/einando/library_api/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.einando.library_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
