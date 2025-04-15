9b81e49a684776fd8a0146655cbea980d4606fdf
Fernando Lopes <87499075+einand0@users.noreply.github.com>
2025-04-15 16:13:41 -0300

    feat: entity User created

---
+++ src/main/java/com/einando/library_api/entities/User.java
@@ -0,0 +1,25 @@
+package com.einando.library_api.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID user_id;
+    private String name;
+    private String email;
+    private String phone;
+}
